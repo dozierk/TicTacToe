bf1e29b7115015f7c5809177ad71cb8014f1a18a
kayla dozier <kayla.dozier82@gmail.com>
2018-02-27 13:30:34 -0800

    Project added to public Git account.

--- Tic Tac Toe/AI.swift
+++ Tic Tac Toe/AI.swift
@@ -11,9 +11,7 @@ import GameplayKit
 
 class AI: NSObject, GKGameModel, GKGameModelPlayer, GKGameModelUpdate {
     
-    //
-    
-    //This is the file where all AI implementation is handled. Initially a start with satisfying all required functions and protocols and each of the variables. Set up Didn't have much time to go further with this part, but there's plenty of potential code. Ran into a hangup when trying to have the AI class conform to NSObject, GKGameModel, GKGameModelPlayer, and GKGameModelUpdate. My thoughts were to then remove the protocols and created extensions. Instead creating classes to separate the model logic to house data required to function and actually implement movement logic in the Move class. In the future, will work on GKGameModel implement and having the AI class conform to that protocol. Should make the code a bit cleaner and concise.
+    //This is the file where all AI implementation is handled. Initially a start with satisfying all required functions and protocols and each of the variables. Didn't have much time to go further with this part, but there's plenty of potential code. Ran into a hangup when trying to have the AI class conform to NSObject, GKGameModel, GKGameModelPlayer, and GKGameModelUpdate. My thoughts were to then remove the protocols and created extensions. Instead creating classes to separate the model logic to house data required to function and actually implement movement logic in the Move class. In the future, will work on GKGameModel implement and having the AI class conform to that protocol. Should make the code a bit cleaner and concise.
     
     var playerId: Int
     
@@ -43,31 +41,28 @@ class AI: NSObject, GKGameModel, GKGameModelPlayer, GKGameModelUpdate {
     
     
     //    Property configuration for gameplay behavior.
-    override init() {
-        super.init()
-        
-        playerId = 1
-        strategist.maxLookAheadDepth = 3
-        strategist.randomSource = nil
-        
-        
-    }
-    
-    
-    //  This class handles any possible move projections.
-    class Move: NSObject, GKGameModelUpdate {
-        
-        var value: Int = 0
-        
-        
-        
-    }
-    
-
+//    override init() {
+//        super.init()
+//
+//        playerId = 1
+//        strategist.maxLookAheadDepth = 3
+//        strategist.randomSource = nil
+//
+//
+//    }
+//
     
     
 }
 
+//  This class handles any possible move projections.
+class Move: NSObject, GKGameModelUpdate {
+    
+    var value: Int = 0
+    
+    
+    
+}
 
 
 
